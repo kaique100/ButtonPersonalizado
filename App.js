bb2e53fa5ea91def0ef0edad4d84ff85a19b570b
kaique100 <skaique5424@gmail.com>
2024-08-21 22:28:37 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
 
-export default function App() {
-  return (
+
+const App = () =>{
+
+  //Função chamada quando um dos botões e precionado
+  const handleButtonPress = () =>{
+    alert("Marcelo é gay");
+  };
+
+  //Retorna a interface do usuario do componente
+  return(
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>
+        botao em react
+      </Text>
+
+      <Button 
+        title='Botão do arrombado do Marcelo' 
+        onPress={handleButtonPress}>
+          botao do Marcelo
+      </Button>
+
+      <TouchableOpacity style={styles.customButton} onPress={handleButtonPress}>
+      <Text style={styles.buttonText}>
+        botao personalizado
+      </Text>
+      </TouchableOpacity>
     </View>
-  );
+  )
+
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
+    container:{
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+
+    title:{
+      fontSize: 20,
+      marginBottom: 10,
+      color: '#000'
+    },
+
+    customButton:{
+      backgroundColor: 'blue',
+      padding: 20,
+      borderRadius: 5
+    },
+
+    buttonText: {
+      color: 'white',
+      fontSize: 18
+    }
+
+})
+
+export default App;
